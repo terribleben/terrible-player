78b208ad900efe269c71647c7443d7ab74be616c
Ben Roth <spongebucket@gmail.com>
2014-10-11 18:27:18 -0700

    more reliable turntable animation

--- iOS/terrible-player/nowplaying/TBPPlayPauseView.m
+++ iOS/terrible-player/nowplaying/TBPPlayPauseView.m
@@ -2,6 +2,8 @@
 //  TBPPlayPauseView.m
 //  terrible-player
 //
+//  Use a CADisplayLink internally because [UIView animate] isn't reliable with repeating animations and app backgrounding
+//
 //  Created by Ben Roth on 10/9/14.
 //  Copyright (c) 2014 Ben Roth. All rights reserved.
 //
@@ -9,12 +11,20 @@
 #import "TBPPlayPauseView.h"
 #import "TBPConstants.h"
 
+#define TBP_TURNTABLE_ACCEL M_PI
+#define TBP_TURNTABLE_RPS ((33.0f / 60.0f) * M_PI * 2.0f)
+
 @interface TBPPlayPauseView ()
+{
+    CGFloat platterVelocity;
+}
 
 @property (nonatomic, strong) UIImageView *vPlatter;
+@property (nonatomic, strong) CADisplayLink *animator;
 
 - (void) startAnimating;
 - (void) stopAnimating;
+- (void) animateFrame: (CADisplayLink *)link;
 
 @end
 
@@ -23,6 +33,8 @@
 - (id) initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
+        platterVelocity = 0;
+        
         self.clipsToBounds = NO;
         self.opaque = NO;
         self.backgroundColor = [UIColor clearColor];
@@ -35,41 +47,53 @@
     return self;
 }
 
-- (void) layoutSubviews
-{
-    [super layoutSubviews];
-    
-    // _vPlatter.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
-}
-
 - (void) setIsPlaying:(BOOL)isPlaying
 {
     if (isPlaying != _isPlaying) {
         if (isPlaying)
             [self startAnimating];
-        else
-            [self stopAnimating];
+        // stopAnimating called once turntable stops
     }
     _isPlaying = isPlaying;
 }
 
 - (void) startAnimating
 {
-    // animation for 1/3 of a revolution
-    // 33 RPM = (60/33) SPR
-    _vPlatter.transform = CGAffineTransformIdentity;
-    [UIView animateWithDuration:(60.0f / (33.0f * 3.0f)) delay:0.0f options:(UIViewAnimationOptionRepeat | UIViewAnimationOptionCurveLinear)
-                     animations:^{
-                         _vPlatter.transform = CGAffineTransformMakeRotation(M_PI * (2.0f / 3.0f));
-                     } completion:nil];
+    if (!_animator) {
+        self.animator = [CADisplayLink displayLinkWithTarget:self selector:@selector(animateFrame:)];
+        [_animator addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+    }
 }
 
 - (void) stopAnimating
 {
-    [UIView animateWithDuration:(60.0f / 33.0f) * (0.99f / 3.0f) delay:0.0f options:(UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseOut)
-                     animations:^{
-                         _vPlatter.transform = CGAffineTransformMakeRotation(M_PI * (1.99f / 3.0f));
-                     } completion:nil];
+    if (_animator) {
+        [_animator invalidate];
+        _animator = nil;
+    }
+}
+
+- (void) animateFrame:(CADisplayLink *)link
+{
+    CGFloat dt = link.duration * (CGFloat)link.frameInterval;
+    
+    // if playing, accelerate the turntable up to 33 RPM
+    if (_isPlaying && platterVelocity < TBP_TURNTABLE_RPS) {
+        platterVelocity += (TBP_TURNTABLE_ACCEL * dt);
+        if (platterVelocity > TBP_TURNTABLE_RPS)
+            platterVelocity = TBP_TURNTABLE_RPS;
+    }
+    // if stopped, slow it to zero and then kill the display link
+    if (!_isPlaying && platterVelocity > 0) {
+        platterVelocity *= 0.95f;
+        if (platterVelocity < 0.02f) {
+            platterVelocity = 0;
+            [self stopAnimating];
+        }
+    }
+    
+    CGFloat singleFrameRotation = platterVelocity * dt;
+    _vPlatter.transform = CGAffineTransformRotate(_vPlatter.transform, singleFrameRotation);
 }
 
 @end
